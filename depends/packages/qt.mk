3a33452fd75a28c032a03785ec017991748bf894
kunalbarchha <kunal.barchha1285@gmail.com>
2021-03-04 14:04:08 +0530

    Update qt.mk

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -1,8 +1,8 @@
 PACKAGE=qt
 $(package)_version=5.2.1
-$(package)_download_path=http://download.qt-project.org/official_releases/qt/5.2/$($(package)_version)/single
-$(package)_file_name=$(package)-everywhere-opensource-src-$($(package)_version).tar.gz
-$(package)_sha256_hash=84e924181d4ad6db00239d87250cc89868484a14841f77fb85ab1f1dbdcd7da1
+$(package)_download_path=https://download.qt.io/new_archive/qt/5.2/$($(package)_version)/single
+$(package)_file_name=$(package)-everywhere-opensource-src-$($(package)_version).tar.xz
+$(package)_sha256_hash=05559100acdff342e9965b8c27ef674d9106f76909c82f94280bbfd46ecf784e
 $(package)_dependencies=openssl
 $(package)_linux_dependencies=freetype fontconfig dbus libxcb libX11 xproto libXext
 $(package)_build_subdir=qtbase
